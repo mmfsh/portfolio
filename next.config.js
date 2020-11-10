1b73be92d963735f8fda52967869c7fae6e9f436
Leo Sammarco <leosammarco@hotmail.com>
2020-11-10 18:24:12 +0000

    Added redirects and updated CV.

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/outbound/booking',
+        destination: 'https://transport.booking.com/search?endLatitude=-12.11923780&endLongitude=-77.02784300&startLatitude=-12.02277900&startLongitude=-77.10642700',
+        permanent: true,
+      },
+      {
+        source: '/outbound/skylab',
+        destination: 'https://www.redbulladvancedtechnologies.com/',
+        permanent: true,
+      },
+      {
+        source: '/outbound/ukfast',
+        destination: 'https://techmanchester.co.uk/',
+        permanent: true,
+      },
+    ]
+  },
+}
